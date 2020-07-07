ba2239e80d480316215f700a4edfefe266c468f2
Soumajit Choudhuri <43232595+Soumajit2000@users.noreply.github.com>
2020-07-07 16:05:14 +0530

    Update App.js
    edited the code

--- src/App.js
+++ src/App.js
@@ -34,33 +34,33 @@ class App extends Component {
     }
   }
 
-  onInputChange = (event) => {
-    this.setState({input: event.target.value});
+   calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol : clarifaiFace.left_col * width,
+      topRow :  clarifaiFace.top_row * height,
+      rightCol :  width - (clarifaiFace.right_col * width),
+      bottomRow :  height - (clarifaiFace.bottom_row * height),
+    }
   }
-
-  calculateFaceLocation = (data) => {
-     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-     const image = document.getElementById('inputImage');
-     const width = Number(image.width);
-     const height = Number(image.height);
-     return {
-       leftCol: clarifaiFace.left_col * width,
-       topRow: clarifaiFace.top_row * height,
-       rightCol: width - (clarifaiFace.right_col * width),
-       bottomRow: height - (clarifaiFace.bottom_col * height),
-     }
-  }
-
+  
   displayFaceBox = (box) => {
-    this.setState({box: box})
-  }
-
+        this.setState({box: box})
+      }
+      
+   onInputChange = (event) => {
+          this.setState({input: event.target.value});
+        }
+  
   onButtonSubmit = () => {
-    this.setState({imageUrl : this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-      .catch(error => console.log(error))
-  }
+      this.setState({imageUrl : this.state.input});
+         app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3',this.state.input)
+            .then(response => this.displayFaceBox(this.calculateFaceLocation(response))) 
+            .catch(err => console.log(err));
+    }
 
   render() {
     return (
